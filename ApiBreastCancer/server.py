9288443e9ac47a60af8fd06a64090fdd99dfbd9f
SergioManuelJob <suarezsergiomanuel69@gmail.com>
2024-01-19 00:10:44 +0000

    Server predicts

--- ApiBreastCancer/server.py
+++ ApiBreastCancer/server.py
@@ -3,6 +3,8 @@ import requests
 import json
 from flask import Flask, request, jsonify
 import pickle
+import warnings
+warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
@@ -10,7 +12,8 @@ model = pickle.load(open('model.pkl','rb'))
 @app.route('/api',methods=['POST'])
 def predict():
     data = request.get_json(force=True)
-    prediction = model.predict([[np.array(data['exp'])]])
+    texture, area, smoothness, concavity = data["texture_worst"], data["area_worst"], data["smoothness_worst"], data["concavity_worst"]
+    prediction = model.predict([[texture, area, smoothness, concavity]])
     output = prediction[0]
     return jsonify(output)
 
