8e951d3a21566b16d8c258065c786a3ec88dd325
SergioManuelJob <suarezsergiomanuel69@gmail.com>
2024-01-22 17:32:09 +0000

    Frontend design fixed, inputs controlled

--- frontend/src/Formulario/Formulario.js
+++ frontend/src/Formulario/Formulario.js
@@ -12,7 +12,13 @@ const Formulario = () => {
   const [prediction, setPrediction] = useState(null);
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+
+    const numberRegex = /^-?\d*\.?\d+$/;
+
+    if (numberRegex.test(value) || value === '') {
+      setFormData({ ...formData, [name]: value });
+    }
   };
 
   const handleSubmit = async (e) => {
@@ -32,36 +38,47 @@ const Formulario = () => {
   };
 
   return (
-    <div>
+    <div className='Contenedor'>
       <h1>Formulario de Predicción</h1>
       <form onSubmit={handleSubmit}>
+
+      <div className="input-group">
         <label>
           Texture Worst:
           <input type="number" name="texture_worst" value={formData.texture_worst} onChange={handleChange} />
         </label>
-        <br />
+      </div>
+
+      <div className="input-group">
         <label>
           Area Worst:
           <input type="number" name="area_worst" value={formData.area_worst} onChange={handleChange} />
         </label>
-        <br />
+      </div>
+
+      <div className="input-group">
         <label>
           Smoothness Worst:
           <input type="number" name="smoothness_worst" value={formData.smoothness_worst} onChange={handleChange} />
         </label>
-        <br />
+      </div>
+
+      <div className="input-group">
         <label>
           Concavity Worst:
           <input type="number" name="concavity_worst" value={formData.concavity_worst} onChange={handleChange} />
         </label>
-        <br />
+      </div>
+
         <button type="submit">Predecir</button>
       </form>
 
-      <div>
-        <h2>Resultado de la Predicción:</h2>
-        <p>{prediction}</p>
-      </div>
+      {prediction && (
+        <div className='resultado'>
+          <h2>Resultado de la Predicción:</h2>
+          <p>{prediction}</p>
+        </div>
+      )}
     </div>
   );
 };
